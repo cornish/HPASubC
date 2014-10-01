748b87839c39f6e6362ab63b617252c5c4722070
cornish <tcornish@gmail.com>
2014-10-01 09:53:41 -0400

    Fix issue with 32 bit Java on 64 bit Windows
    - added checking in the registry for
    HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\JavaSoft\Java Runtime Environment
    in addition to HKLM\SOFTWARE\JavaSoft\Java Runtime Environment
    
    - not thoroughly tested on system with this arrangment, but still working
    on 64bit windows with 64bit Java

--- runAnalyzeImages.bat
+++ runAnalyzeImages.bat
@@ -11,20 +11,38 @@
 cls
 setlocal ENABLEEXTENSIONS
 set KEY_NAME="HKLM\SOFTWARE\JavaSoft\Java Runtime Environment"
+set WOW_KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\JavaSoft\Java Runtime Environment"
 set VALUE_NAME=CurrentVersion
 ::
 :: get the current version
 ::
+:: check for the version that is native to 32 bit or 64 bit Windows
 FOR /F "usebackq skip=2 tokens=3" %%A IN (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
     set ValueValue=%%A
 )
+
 if defined ValueValue (
-    @echo the current Java runtime is  %ValueValue%
+    :: we found the native
+    echo %KEY_NAME%\%VALUE_NAME% found.
+    echo the current Java runtime is  %ValueValue%
+    set JAVA_CURRENT="HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\%ValueValue%"
 ) else (
-    @echo %KEY_NAME%\%VALUE_NAME% not found.
-    goto end
+    echo %KEY_NAME%\%VALUE_NAME% not found.
+    :: check for a 32 bit Java on a 64 bit Windows (WOW)
+    FOR /F "usebackq skip=2 tokens=3" %%A IN (`REG QUERY %WOW_KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
+        set ValueValue=%%A
+    )
+    if defined ValueValue (
+        :: we found the 32 bit version in WOW
+        echo %WOW_KEY_NAME%\%VALUE_NAME% found.
+        echo the current Java runtime is  %ValueValue%
+        set JAVA_CURRENT="HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\JavaSoft\Java Runtime Environment\%ValueValue%"
+    ) else (
+        echo %WOW_KEY_NAME%\%VALUE_NAME% not found.
+        goto end
+    )
 )
-set JAVA_CURRENT="HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\%ValueValue%"
+
 set JAVA_HOME=JavaHome
 ::
 :: get the javahome
@@ -32,6 +50,9 @@ set JAVA_HOME=JavaHome
 FOR /F "usebackq skip=2 tokens=3*" %%A IN (`REG QUERY %JAVA_CURRENT% /v %JAVA_HOME% 2^>nul`) DO (
     set JAVA_PATH=%%A %%B
 )
+
+
+:: presumably we have found the path in the registry or quit
 echo the path of the current Java JVM according to the registry is
 echo %JAVA_PATH%
 echo.
